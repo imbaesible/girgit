c8682a0663f4e9cd5a559663a8c378bf594cf2ef
Hemant Ashta <hemantashta@Hemants-MacBook.local>
2023-02-04 00:23:52 +0530

    Authentication and Autherization Setup

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+import authRoutes from "./routes/auth.js";
+import { register } from "./controllers/auth.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -35,6 +37,14 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+// Routes with files 
+
+app.post("/auth/register", upload.single("picture"), register);
+
+// Routes 
+
+app.use("/auth", authRoutes);
 
 //MONGOOSE SETUP
 
